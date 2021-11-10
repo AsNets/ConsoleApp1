553d0bf9d331832cdf43e790f79ddba33f065bbd
AsNets <asnets.software@gmail.com>
2021-11-10 15:16:30 +0300

    console kodu eklendi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
       public  static void Main(string[] args)
         {
             Console.WriteLine("Merhaba ASNETS");
+            Console.WriteLine("Bu bir deneme uygulamasıdır");
             Console.ReadLine();
             
         }
