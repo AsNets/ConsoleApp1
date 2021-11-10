98620e0cfc18ef45f1af0edabf2a5f038827f6c5
AsNets <asnets.software@gmail.com>
2021-11-10 15:25:26 +0300

    EkranaBas fonksiyonu eklendi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,18 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+      
       public  static void Main(string[] args)
         {
+              string EkranaBas()
+            {
+                return "Fonksiyon çalıitı";
+            }
+
             Console.WriteLine("Merhaba ASNETS");
             Console.WriteLine("Bu bir deneme uygulamasıdır");
+            EkranaBas();
             Console.ReadLine();
             
         }
